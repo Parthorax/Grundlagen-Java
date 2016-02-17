c106252b84baf39e05d207cfd4988e29987cb2ed
Parthorax <da.funky.rad@gmail.com>
2016-02-17 15:15:11 +0100

    Tag 3

--- Aufgabenblatt2/src/KorrekteLoesung/Prog.java
+++ Aufgabenblatt2/src/KorrekteLoesung/Prog.java
@@ -11,6 +11,7 @@ public class Prog {
 		System.out.println(ford.getPs());
 		
 		//Aufgabe 2
+		System.out.println("----------------Aufgabe 2------------------------");
 		Pkw b = new Pkw();
 		System.out.println(b.getPs());
 		
@@ -21,6 +22,7 @@ public class Prog {
 		System.out.println("Farbe= "+bmw.getFarbe());
 		
 		//Aufgabe 3
+		System.out.println("----------------Aufgabe 3------------------------");
 		int [] psListe = {50,60,80,120,140};
 		int [] vmaxListe = {120,150,180,200,210};
 		int [] tankListe = {40, 50, 60, 70, 80};
@@ -43,6 +45,7 @@ public class Prog {
 		 }
 			 
 		//Aufgabe 4
+		 System.out.println("----------------Aufgabe 4------------------------");
 		Fahrzeug mazda = new Fahrzeug (50,120,50,"rot");
 		double d = mazda.fahren(250);
 		System.out.println("Verbrauch = "+d);
@@ -53,37 +56,43 @@ public class Prog {
 		System.out.println("Farbe= "+mazda.getFarbe());
 		
 		//Aufgabe 5
+		System.out.println("----------------Aufgabe 5------------------------");
 		mazda.setPs(-1);
 		mazda.setPs(301);
 		
 		mazda.setFarbe(null);
 		
 		//Aufgabe 6
+		System.out.println("----------------Aufgabe 6------------------------");
 		mazda.tanken(59);
 		System.out.println(mazda.getTank());
 		
 		//Aufgabe 7
+		System.out.println("----------------Aufgabe 7------------------------");
 		int kw = mazda.psZuKw(90);
 		System.out.println("90 Ps = "+kw+" kw");
 			 
 		//Tag 3 Aufgabe 2
+		System.out.println("----------------TAG 3 Aufgabe 2------------------------");
 		Pkw b3 = new Pkw(50, 90, 100, "rot");
 		double d3 = b3.fahren(250);
 		System.out.println("Verbrauch Aufgabe 2 = "+d3+" der tank "+b3.getTank());
 		
 		// Tag 3 Aufgabee 3
 		
+		System.out.println("----------------TAG 3 Aufgabe 3------------------------");
 		Pkw p3 = new Pkw();
 		double l3 = p3.fahren(250, 5);  //hier geben wir 2 parameter über, daher weiß er das wir die case methode wollen
 		System.out.println("Verbrauch Aufgabe 3 = "+l3+" der tank "+b3.getTank());
 		
 		
 		//Tag 3 Aufgabe 4
+		System.out.println("----------------TAG 3 Aufgabe 4------------------------");
 		Pkw neu = new Pkw(); //Test erstellung
 		System.out.println("So viele Autos wurden erstellt: "+Pkw.zaehler);
 		
 		// Aufgabe 6
-		
+		System.out.println("----------------TAG 3 Aufgabe 6------------------------");
 		String s1 = "Hallo Welt";
 		String s2 = "Hallo";
 		boolean kk = s1.equals(s2);
